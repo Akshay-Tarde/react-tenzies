ee1082a924a5b2e10b20aa1886531fa0deaab443
Akshay-Tarde <akshaymtarde@gmail.com>
2025-01-17 17:59:35 +0530

    Made some tweaks to the die styles. Set the State as array of 10 random numbers. Removed the hard coded Die components and replaced it with a map logic.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,33 @@
+import React from "react"
 import Die from "./Die"
 
 export default function App() {
+    function generateAllNewDice() {
+        const newDice = []
+        
+        for ( let i = 0; i < 10; i++) {
+            const randNum = Math.floor(Math.random() * 7)
+            newDice.push(randNum)
+        }
+        return newDice
+    }
+    const [diceArray, setDiceArray] = React.useState(generateAllNewDice())
+    
+
+    // functional programming approach
+    // function generateAllNewDice() {
+    //     return new Array(10)
+    //         .fill(0)
+    //         .map(() => Math.ceil(Math.random() * 6))
+    // }
+
+    console.log(diceArray)
+
+    const dieMap = diceArray.map(item => <Die value={item}/> )
     return (
         <main>
             <div className="dice-container">
-                <Die className="die" value={1}/>
-                <Die className="die" value={2}/>
-                <Die className="die" value={3}/>
-                <Die className="die" value={4}/>
-                <Die className="die" value={5}/>
-                <Die className="die" value={6}/>
-                <Die className="die" value={4}/>
-                <Die className="die" value={3}/>
-                <Die className="die" value={5}/>
-                <Die className="die" value={1}/>
+                {dieMap}
             </div>
         </main>
     )
