a7243448540e052cd75d93b6c22d3c81c6ecc1ad
Akshay-Tarde <akshaymtarde@gmail.com>
2025-01-17 17:33:12 +0530

    Created the Die component and styled it according to the figma file.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,20 @@
+import Die from "./Die"
+
 export default function App() {
     return (
-        <main>This is the App component!</main>
+        <main>
+            <div className="dice-container">
+                <Die className="die" value={1}/>
+                <Die className="die" value={2}/>
+                <Die className="die" value={3}/>
+                <Die className="die" value={4}/>
+                <Die className="die" value={5}/>
+                <Die className="die" value={6}/>
+                <Die className="die" value={4}/>
+                <Die className="die" value={3}/>
+                <Die className="die" value={5}/>
+                <Die className="die" value={1}/>
+            </div>
+        </main>
     )
 }
